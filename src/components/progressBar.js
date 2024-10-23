892e5b275f5c6342524ef594184ccf4ce2714721
peponi <peponi_paradise@naver.com>
2024-10-23 13:19:26 +0900

    feat: progress bar title 부여

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -4,7 +4,7 @@ const progressClassName = "h-2 rounded-full bg-sky-600 ";
 export default function ProgressBar({ className = "", progressPercent = 50 }) {
   let progressValue = `w-[${progressPercent}%]`;
   return (
-    <div className={outlineClassName + className}>
+    <div className={outlineClassName + className} title={`${progressPercent}%`}>
       <div className={progressClassName + progressValue}></div>
     </div>
   );
