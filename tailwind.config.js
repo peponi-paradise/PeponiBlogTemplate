72bb81ded7ee91f70d2e2f358015733ac2910a51
peponi <peponi_paradise@naver.com>
2024-10-30 12:45:38 +0900

    feat: Tailwind mdx support
    mdx 파일에 tailwind 사용 시 지원될 수 있도록 경로 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./posts/**/*.mdx",
+    "./projects/*.mdx",
   ],
   theme: {
     extend: {},
